19716045bf361b8e515250222402f42e13f9a156
Alejandro Castellon <alejandrocastellonfer@gmail.com>
2024-05-02 10:11:24 -0400

    add customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,24 @@
 import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import { Suspense } from 'react';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+
+  const query = searchParams?.query || '';
+    return (
+      <Suspense fallback={<TableRowSkeleton />}>
+        <CustomersTable query={query}/>
+      </Suspense>
+    )
+}
